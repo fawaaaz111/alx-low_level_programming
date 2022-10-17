8b0ccf2af66bfd2607a30e57cd8c996d2ae82da3
fawaaaz111 <fawaztwix111@gmail.com>
2022-10-17 15:31:53 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/*
- * main - Determine if the number is positive , negative or zero
+/**
+ * main - Prints a random number and states whether
+ * it is positive, negative, or zero.
  *
- * Return : 0 if success
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -13,13 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
-	
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	
-	else   printf("%d is zero\n", n);
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
