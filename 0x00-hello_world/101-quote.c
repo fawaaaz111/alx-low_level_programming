700d414efb638f63540a9c4d978245e34a1df4b2
fawaaaz111 <fawaztwix111@gmail.com>
2022-10-17 00:40:49 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
 /**
  *  * main - Entry point
  *   * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 		write(STDOUT_FILENO, "and that piece of art is useful
