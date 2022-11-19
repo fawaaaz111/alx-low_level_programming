98ba64025881ea21b2b7762ba9814b0cd1c50628
fawaaaz111 <fawaztwix111@gmail.com>
2022-11-19 07:43:02 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - pritn from input number to 90
+ * @n: the input
+ *
+ * Return: Void
+ */
+
+void print_to_98(int n)
+{
+	int cnt; /* counter hold n argument */
+
+	for (cnt = n ; cnt <= 98 ; cnt++)
+	{
+		printf("%d", cnt);
+		if (cnt != 98)
+		{
+			printf(", ");
+		}
+		putchar('\n');
+	}
+}
