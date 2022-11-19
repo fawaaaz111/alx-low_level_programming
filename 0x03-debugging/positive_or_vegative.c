0fc85b9eac29b1e950c3fb26311ad5c02f686aa6
fawaaaz111 <fawaztwix111@gmail.com>
2022-11-19 18:25:34 +0000

    Based on the main.c file above, create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.

--- 0x03-debugging/positive_or_vegative.c
+++ 0x03-debugging/positive_or_vegative.c
@@ -8,12 +8,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	/*srand(time(0)); */
+	/* n = rand() - RAND_MAX / 2; */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
