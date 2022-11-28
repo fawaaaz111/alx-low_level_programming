86f759f867fc3fdb204225dd65d295aa2b12f43e
fawaaaz111 <fawaztwix111@gmail.com>
2022-11-28 01:05:01 -0800

    Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,9 +12,10 @@ void print_times_table(int n)
 	if (!(n < 0 || n > 15))
 	{
 		int con, time; /* initilaize row counter,and time number */
-		int number, fir, sec, mid; /** 
+		int number, fir, sec, mid;
+		/**
 					      * mid is the middle if 3 digi
-					      * number 
+					      * number
 					      */
 
 		con = n;
@@ -23,9 +24,47 @@ void print_times_table(int n)
 		{
 			for (time = 0 ; time <= n ; time++)
 			{
-				print_number(con , time);
+				print_number(con, time);
 			}
 		_putchar('\n');
 		}
+	}
+}
+
+/**
+  * print_number - print the number of 2 or 3 digits
+  * @con: the row counter
+  * @time: number of time to multiply
+  * Return: none
+ */
+
+void print_number(int con, int time)
+{
+	int number = con * time;
+	int fir, sec;
+
+	fir = number / 10;
+	sec = number % 10;
+
+	if (fir != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(fir + '0');
+		_putchar(sec + '0');
+	}
+	else
+	{
+		if (time != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	_putchar(sec + '0');
+	}
+	if (time != 0)
+	{
+		_putchar (',');
 	}
 }
