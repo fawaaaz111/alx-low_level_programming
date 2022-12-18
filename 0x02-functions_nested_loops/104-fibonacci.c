29ec14abca556ed4cc46edee6d48e7def1863a19
fawaaaz111 <fawaztwix111@gmail.com>
2022-12-18 02:45:29 -0800

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 
 /**
-  * main - prints first 98 fibonacci numbers
+  * main - prints the first 98 fibonacci numbers
   *
-  * Return: 0 on success
+  * Return: Always 0
   */
 
 int main(void)
 {
-	int i; /* counter */
-	unsigned long int pre = 2, n_pre = 1, num;
-	/* previous and next previous number */
+	int c, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	printf("%ld, %ld", n_pre, pre);
-	for (i = 0; i < 96; i++)
+	n1 = 1;
+	n2 = 2;
+	boolean = boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		num = pre + n_pre;
-		printf(", %ld", num);
-		n_pre = pre;
-		pre = num;
+		if (boolean)
+		{
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
+		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
 	printf("\n");
 	return (0);
