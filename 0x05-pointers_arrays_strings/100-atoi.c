863f5ea9773ecc842c268e27cdbb2bb6ce23d36e
fawaaaz111 <fawaztwix111@gmail.com>
2022-12-22 15:27:10 +0000

    Write a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -29,10 +29,6 @@ int _atoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i - 1] == '-' || s[i - 1] == '+')
-			{
-				opt += _putchar(s[i - 1]);
-			}
 			opt = opt * 10 + (s[i] - '0');
 		}
 		i++;
