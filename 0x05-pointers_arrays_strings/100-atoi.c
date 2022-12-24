8afc52f5aec03e874940441094935fe6239822c1
fawaaaz111 <fawaztwix111@gmail.com>
2022-12-24 06:17:19 +0000

    Write a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,17 +12,14 @@ int _atoi(char *s)
 	int i, opt = 0; /* counter for loop and the output */
 	int sign_count = 0, sign = 1;
 
-	for (i = 0 ; s[i]  != '\0'; i++)
+	i = 0;
+	while (s[i] < '0' || s[i] > '9')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-			break;
-
-		else if (s[i] == '-')
-		{
+		if (s[i] == '-')
 			sign_count++;
-			if ((sign_count % 2) != 0)
-				sign = -1;
-		}
+		if ((sign_count % 2) != 0)
+			sign = -1;
+		i++;
 	}
 	i = 0;
 	while (s[i] != '\0')
