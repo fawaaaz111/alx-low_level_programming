a2a49321245427ad8fbdf885856edbeef7381a4f
fawaaaz111 <fawaztwix111@gmail.com>
2022-12-25 13:04:16 +0000

    Write a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,6 +12,7 @@ int _atoi(char *s)
 	unsigned int i, opt = 0; /* counter for loop and the output */
 	unsigned int sign = 1;
 
+	/* loop till first number to set the sign */
 	i = 0;
 	while (s[i] < '0' || s[i] > '9')
 	{
@@ -21,12 +22,15 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+	/* find just the first set of number */
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+		/* shift original number by one to the left first the add */
 			opt = opt * 10 + (s[i] - '0');
+		/* check next item if a number 'continue loop' or not 'break */
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
