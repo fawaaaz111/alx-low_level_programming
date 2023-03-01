b0e09772fb87e781d6682347a6dfce95ef465b99
fawaaaz111 <fawaztwix111@gmail.com>
2023-03-01 13:18:48 +0000

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,28 @@
 #include "main.h"
-#include <string.h>
 
 /**
-* _strncat -> function to append some charx
-* @dest: first param
-* @src: second param
-* @n: 3rd param
-* Return: string
-*/
+  * _strncat - this is a fuction strcat
+  * @dest: first parameter
+  * @src: second parameter
+  * @n: number of characters to take from str
+  *
+  * Return: a string
+  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int len, i;
+
+	/* reach the end of destination passage */
+	len = 0;
+	while (dest[len] != '\0')
+		len++;
+
+	for (i = 0; src[i] != 0 && i < n; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
