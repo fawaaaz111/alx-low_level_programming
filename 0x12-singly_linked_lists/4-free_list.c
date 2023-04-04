03c0920398862078c3606ab84f8e4f5d8942404f
fawaaaz111 <fawaztwix111@gmail.com>
2023-04-04 07:13:02 +0000

    Write a function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,12 @@
 #include "lists.h"
 
-/**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
- */
-
 void free_list(list_t *head)
 {
-	list_t *current;
-
-	while ((current = head) != NULL)
-	{
-		head = head->next;
-		free(current->str);
-		free(current);
-	}
+	/*base case: stop when reaching end of list*/
+	if (head == NULL)
+		return;
+	else
+		/*recursive mode: recall with (next) as new head*/
+		free_list(head->next);
+		free(head);
 }
