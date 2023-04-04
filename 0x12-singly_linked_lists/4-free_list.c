3037ee2fb3e511bc544775541ae2026e8bdee8ae
fawaz_alhafiz <48890670+fawaaaz111@users.noreply.github.com>
2023-04-04 09:16:33 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
 void free_list(list_t *head)
 {
 	/*base case: stop when reaching end of list*/
