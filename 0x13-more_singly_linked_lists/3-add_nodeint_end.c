395be94d845e57f2e912595e106c9a2716a404c6
fawaaaz111 <fawaztwix111@gmail.com>
2023-04-08 14:37:26 +0000

    Write a function that adds a new node at the end of a listint_t list.

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,12 +18,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!ptr)
 		return (NULL);
 
-	/* travers list till the end*/
-	if (cur->next)
-		cur = cur->next;
 	ptr->n = n;
 	ptr->next = NULL;
-	cur->next = ptr;
+
+	/*if in the beginning of a list, assign directly*/
+	if (*head == NULL)
+		*head = ptr;
+	else
+	{
+
+		/* travers list till the end*/
+		while (cur->next)
+			cur = cur->next;
+
+		cur->next = ptr;
+	}
 
 	return (*head);
 }
