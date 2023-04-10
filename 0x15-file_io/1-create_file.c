3989d9e95cc9d8f3ff107e6d715c85f44c1e88d4
fawaz_alhafiz <48890670+fawaaaz111@users.noreply.github.com>
2023-04-10 17:12:44 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	if (filename == NULL)
