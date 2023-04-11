8e5e7f1ed8740e9a164a1c4e5f6b573fd0673b90
fawaaaz111 <fawaztwix111@gmail.com>
2023-04-11 06:28:45 +0000

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,12 +19,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (length = 0; text_content[length]; length++)
-		;
+	if (text_content)
+	{
+		for (length = 0; text_content[length]; length++)
+			;
 
-	l_write = write(fd, text_content, length);
-	if (l_write == -1)
-		return (-1);
-
+		l_write = write(fd, text_content, length);
+		if (l_write == -1)
+			return (-1);
+	}
 	return (1);
 }
